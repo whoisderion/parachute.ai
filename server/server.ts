6f16cd602cdce70ff2dd542789dda66c5ef8a190
Derion Williams <derionwllms@gmail.com>
2023-03-21 01:03:04 -0500

    Added route for transcribing local recordings

--- server/server.ts
+++ server/server.ts
@@ -1,17 +1,17 @@
 require('dotenv').config()
-import express, { Request, Response } from 'express'
+import express, { Request, response, Response } from 'express'
 //import OpenAI from 'openai-api'
 import { Configuration, OpenAIApi } from "openai"
 import dotenv from 'dotenv'
 import axios from 'axios'
+import fs from 'fs'
 
 const app = express()
 
-const OpenaiApiKey = "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"
+const OpenaiApiKey = "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"
 const OpenaiOrgID = "org-PRuztropUTgrHaRBj8thsiy0"
 
 const OpenaiConfiguration = new Configuration({
-    organization: OpenaiOrgID,
     apiKey: OpenaiApiKey,
 });
 
@@ -27,28 +27,33 @@ app.get('/', (req: Request, res: Response) => {
 })
 
 app.get('/openai/test', async (req: Request, res: Response) => {
-    const response = await openai.listEngines()
-    console.log(response)
-    return res.sendStatus(200)
+    const response = await openai.listModels();
+    res.send(response.data)
 })
 
-app.get('/openai/test2', async (req: Request, res: Response) => {
-    axios.post("https://api.openai.com/v1/chat/completions", {
+app.get('/openai/transcribe', (req: Request, res: Response) => {
+    const recording: string = 'server/recordings/2022_08_03_10_19AM.mp3'
+    const model: string = 'whisper-1'
+    const prompt: string = 'The transcript is from a customer calling a moving company with a salesman named Dave.'
+    axios.post('https://api.openai.com/v1/audio/transcriptions', {
+        'file': recording,
+        'model': 'whisper-1',
+        'prompt': prompt,
+    }, {
         headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${OpenaiApiKey}`
-        },
-        data: {
-            "model": "gpt-3.5-turbo",
-            "messages": [{ "role": "user", "content": "Say this is a test!" }],
-            "temperature": 0.7
+            'Authorization': `Bearer ${OpenaiApiKey}`,
+            'OpenAI-Organization': 'org-PRuztropUTgrHaRBj8thsiy0'
+
         }
     })
         .then(response => {
-            res.send(response)
+            fs.appendFile('server/output/whisperOutput.txt', JSON.stringify(response), (err) => {
+                console.log('transcription successful for ', recording)
+            })
+            res.send(response.data)
         })
-        .catch(err => {
-            res.send(err)
+        .catch(error => {
+            res.send(error)
         })
 })
 
