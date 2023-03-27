4b862a5587ff0176eed03b7dc2283f68795a24e3
Derion Williams <derionwllms@gmail.com>
2023-03-27 01:03:50 -0500

    Created HTML skeleton to interface with backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='upload-file'>
+        <h3>Upload a recording</h3>
+        <button>X</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className='options'>
+        <div>
+          <p>Summary</p>
+          <input type="checkbox" />
+        </div>
+        <div>
+          <p>Sentiment</p>
+          <input type="checkbox" />
+        </div>
+        <div>
+          <p>Flag</p>
+          <input type="checkbox" />
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className='response'>
+        <h3>Response</h3>
+        <div className='openAI-response'>
+        </div>
+      </div>
     </div>
   )
 }
