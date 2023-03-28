3b7f2818d27f599d4f8e87c630cff1ea36ef3d98
Derion Williams <derionwllms@gmail.com>
2023-03-28 01:21:17 -0500

    Configured Tailwind and added styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,25 +6,25 @@ function App() {
     <div className="App">
       <div className='upload-file'>
         <h3>Upload a recording</h3>
-        <button>X</button>
+        <button>Upload</button>
       </div>
       <div className='options'>
-        <div>
-          <p>Summary</p>
+        <div className='summary block'>
+          <p className='inline-flex mr-2'>Summary</p>
           <input type="checkbox" />
         </div>
-        <div>
-          <p>Sentiment</p>
+        <div className='sentiment block'>
+          <p className='inline-flex mr-2'>Sentiment</p>
           <input type="checkbox" />
         </div>
-        <div>
-          <p>Flag</p>
+        <div className='flag block'>
+          <p className='inline-flex mr-2'>Flag</p>
           <input type="checkbox" />
         </div>
       </div>
-      <div className='response'>
+      <div className='response mt-10'>
         <h3>Response</h3>
-        <div className='openAI-response'>
+        <div className='openAI-response w-96 h-32 border-2 border-slate-200'>
         </div>
       </div>
     </div>
