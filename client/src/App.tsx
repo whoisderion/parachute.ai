81f8b008b2b8d1f78e0f95b22f3f29e8ff0f180f
Derion Williams <derionwllms@gmail.com>
2023-03-29 02:02:01 -0500

    Added Axios & functions to submit data to server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,36 +1,88 @@
+import { useState } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
+
+  const [summary, setSummary] = useState(false)
+  const [sentiment, setSentiment] = useState(false)
+  const [flag, setFlag] = useState(false)
+  const [fileName, setFileName] = useState('')
+  const [transcript, setTranscript] = useState('')
+  const [response, setResponse] = useState('')
+
+  const toggleSummary = () => {
+    setSummary(!summary)
+  }
+
+  const toggleSentiment = () => {
+    setSentiment(!sentiment)
+  }
+
+  const toggleFlag = () => {
+    setFlag(!flag)
+  }
+
+  const handleSubmit = async () => {
+    await getTranscript()
+    await getAnalysis()
+
+  }
+
+  const getTranscript = () => {
+    let prompt = ''
+    if (summary) {
+      prompt = prompt + 'What happened in the following call transcription?'
+    }
+    if (sentiment) {
+      prompt = prompt + 'What is the sentiment of the caller in this transcription?'
+    }
+    if (flag) {
+      prompt = prompt + 'If the sentiment is generally negative end your response with a "<Negative>", otherwise end your response with a "<Not Negative>".'
+    }
+
+    axios.get('http://127.0.0.1:8080/openai/transcribe', { data: { data: prompt } })
+      .then(res => setTranscript(res))
+  }
+
+  const getAnalysis = () => {
+    axios.get('http://127.0.0.1:8080/openai/analyze', { data: { transcript: transcript } })
+      .then(res => setResponse(res))
+  }
 
   return (
     <div className="App">
       <div className='upload-file'>
         <h3>Upload a recording</h3>
+        <p>File: {`${fileName}`}</p>
         <button>Upload</button>
       </div>
       <div className='options mt-4'>
         <div className='summary block'>
-          <p className='inline-flex mr-2'>Summary</p>
-          <input type="checkbox" />
+          <p className='inline-flex mr-2'>Summary {`${summary}`}</p>
+          <input type="checkbox" onChange={toggleSummary} />
         </div>
         <div className='sentiment block'>
-          <p className='inline-flex mr-2'>Sentiment</p>
-          <input type="checkbox" />
+          <p className='inline-flex mr-2'>Sentiment {`${sentiment}`}</p>
+          <input type="checkbox" onChange={toggleSentiment} />
         </div>
         <div className='flag block'>
-          <p className='inline-flex mr-2'>Flag</p>
-          <input type="checkbox" />
+          <p className='inline-flex mr-2'>Flag {`${flag}`}</p>
+          <input type="checkbox" onChange={toggleFlag} />
         </div>
+        <button className='mt-4' onClick={handleSubmit}>Submit for analysis</button>
       </div>
       <div className='result inline-flex mt-10'>
         <div className='transcript mr-4'>
           <h3>Transcript</h3>
           <div className='openAI-response w-96 h-44 p-4 border-2 border-slate-200'>
+            <p>{`${transcript}`}</p>
           </div>
         </div>
         <div className='response ml-4'>
           <h3>Response</h3>
           <div className='openAI-response w-96 h-44 p-4 border-2 border-slate-200'>
+            <p>{`${response}`}</p>
           </div>
         </div>
       </div>
